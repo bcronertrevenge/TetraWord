7330c1784d3c278bfce5ae49c7f6823a5b980c5f
calimarie94 <marie.huertas.pro@gmail.com>
2014-03-30 11:35:05 +0200

    ajout de l'interface

--- src/fr/univ/tetraword/MainGame.java
+++ src/fr/univ/tetraword/MainGame.java
@@ -1,15 +1,96 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package fr.univ.tetraword;
 
-/**
- *
- * @author bruno
- */
+import fr.univ.graphicinterface.JCoolButton;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.Border;
+
 public class MainGame {
     
+    /* Fonctions */
+    public static JPanel setBackgroundImage(JFrame frame, final File img) throws IOException{
+	JPanel panel = new JPanel(){
+            private static final long serialVersionUID = 1;
+            private BufferedImage buf = ImageIO.read(img);
+            
+            @Override
+            protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawImage(buf, 0,0, null);
+            }
+	};
+	
+	frame.setContentPane(panel);
+	return panel;
+    }   
+    
+    /* Programme Principal */
+    public static void main(String[] args) throws IOException{
+        
+            // Création de la fenêtre
+            JFrame frame = new JFrame();
+            frame.setTitle("Tetra Word");
+            frame.setPreferredSize(new Dimension(1024,768));
+                        
+            // Arrière plan
+            JPanel panel = setBackgroundImage(frame, new File("src/fr/univ/graphicinterface/accueil.jpg"));
+            panel.setMaximumSize(new Dimension(1024, 768));
+            panel.setMinimumSize(new Dimension(600, 400));
+            panel.setPreferredSize(new Dimension(1024, 768));
+            
+            // Création des boutons
+            Font century = new Font("Century Gothic",0,26);
+            
+            JCoolButton jouer = new JCoolButton("Jouer");
+            jouer.setPreferredSize(new Dimension(220,60));
+            jouer.setForeground(Color.white);
+            jouer.setFont(century);
+            jouer.setFocusPainted(false);
+                        
+            JCoolButton options = new JCoolButton("Options");
+            options.setPreferredSize(new Dimension(220,60));
+            options.setForeground(Color.white);
+            options.setFont(century);
+            options.setFocusPainted(false);
+                     
+            JCoolButton quitter = new JCoolButton("Quitter");
+            quitter.setPreferredSize(new Dimension(220,60));
+            quitter.setForeground(Color.white);
+            quitter.setFont(century);
+            quitter.setFocusPainted(false);
+       
+            // Placement à gauche
+            GroupLayout jPanel1Layout = new GroupLayout(panel);
+            panel.setLayout(jPanel1Layout);
+            jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGap(124, 124, 124)
+                    .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jouer, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(options, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(quitter, GroupLayout.PREFERRED_SIZE, 260, GroupLayout.PREFERRED_SIZE))
+                    .addContainerGap(640, Short.MAX_VALUE))
+                );
+            
+            jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(370, Short.MAX_VALUE)
+                .addComponent(jouer, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addComponent(options, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(quitter, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+                .addGap(159, 159, 159))
+        );
+           
+            frame.pack();
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
